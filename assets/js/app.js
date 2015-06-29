a5fd34bfd5711bd782c60c043b7c65e177865751
stephan <lishufan00@aliyun.com>
2015-06-30 01:12:32 +0800

    add angular library

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+﻿angular.module('app', [])
+
+.controller('MenuController', function ($scope) {
+    var menus = $scope.menus = [
+        { href:"#",text:"Home",selected:true },
+        { href:"#",text:"About LongFood" ,selected:false},
+        { href: "#", text: "Service Offerings", selected: false },
+        { href: "#", text: "Sales", selected: false },
+        { href: "#", text: "Contact Us", selected: false },
+    ];
+    $scope.select = function (menu) {
+        angular.forEach(menus, function (menu) {
+            menu.selected = false;
+        });
+        menu.selected = true;
+    }
+});
