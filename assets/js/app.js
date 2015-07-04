6aa8fefd9f080f3510c75a1ac33b01e98d662679
longfood@outlook.com <longfood@outlook.com>
2015-07-05 06:10:55 +0800

    refact structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,74 @@
-﻿angular.module('app', [])
+﻿var app = angular.module("longfoodApp", ["ui.router", "longfoodControllers"]);
 
-.controller('MenuController', function ($scope) {
-    var menus = $scope.menus = [
-        { href:"#",text:"Home",selected:true },
-        { href:"#",text:"About LongFood" ,selected:false},
-        { href: "#", text: "Service Offerings", selected: false },
-        { href: "#", text: "Sales", selected: false },
-        { href: "#", text: "Contact Us", selected: false },
-    ];
-    $scope.select = function (menu) {
-        angular.forEach(menus, function (menu) {
-            menu.selected = false;
-        });
-        menu.selected = true;
+//app.config(['$routeProvider',
+//    function ($routeProvider) {
+//        $routeProvider.
+//            when('/about', {
+//                templateUrl: 'views/about.html',
+//                controller: 'AboutCtrl'
+//            }).
+//            when('/contact', {
+//                templateUrl: 'views/contact.html',
+//                controller: 'ContactCtrl'
+//            }).
+//            otherwise({
+//                redirectTo: '/about'
+//            });
+//    }
+//]);
+app.run(
+  ['$rootScope', '$state', '$stateParams',
+    function ($rootScope, $state, $stateParams) {
+
+        // It's very handy to add references to $state and $stateParams to the $rootScope
+        // so that you can access them from any scope within your applications.For example,
+        // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+        // to active whenever 'contacts.list' or one of its decendents is active.
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
     }
-});
+  ]
+)
+.config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+        .state('home', {
+            url: "/:lang",
+            templateUrl: function ($stateParams) {
+                var lang = $stateParams.lang || "en";
+                return "views/" + lang + "/home.html";
+            },
+        })
+        .state('about', {
+            url: "/about/:lang",
+            templateUrl: function ($stateParams) {
+                var lang = $stateParams.lang || "en";
+                return "views/" + lang + "/about.html";
+            },
+            controller: 'AboutCtrl'
+        })
+        .state('contact', {
+            url: "/contact/:lang",
+            templateUrl: function ($stateParams) {
+                var lang = $stateParams.lang || "en";
+                return "views/" + lang + "/contact.html";
+            },
+            controller: 'ContactCtrl'
+        })
+    }
+])
+.service('langService', ['$rootScope', function ($rootScope) {
+    var service = {
+        lang: "en",
+        change: function (newValue) {
+            if (service.lang != newValue) {
+                service.lang = newValue;
+            }
+            $rootScope.$broadcast('lang.changed');
+        }
+    }
+
+    return service;
+}]);
---
+++ assets/js/controllers.js
@@ -0,0 +1,39 @@
+﻿var longfoodControllers = angular.module('longfoodControllers', []);
+
+longfoodControllers.controller('AboutCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.Text = "This is the about page."
+    }
+]);
+
+longfoodControllers.controller('ContactCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.Text = "This is the about page."
+    }
+]);
+
+longfoodControllers.controller('LangController', function ($rootScope, $scope, $state, langService) {
+    var menus = $scope.menus = [
+        { href: "#", lang:"en", text: "English", selected: true },
+        { href: "#", lang:"fi", text: "Finland", selected: false },
+        { href: "#", lang:"ch", text: "简体中文", selected: false },
+    ];
+    $scope.select = function (menu) {
+        angular.forEach(menus, function (menu) {
+            menu.selected = false;
+        });
+        menu.selected = true;
+        langService.change(menu.lang);
+        $state.go($state.current.name, {lang:menu.lang});
+    }
+});
+
+longfoodControllers.controller('MainCtrl', ['$scope', '$state','langService',
+    function ($scope, $state, langService) {
+        $scope.Text = "This is the about page."
+        $scope.$on('lang.changed', function ( event ) {
+            $scope.lang = langService.lang;
+        });
+        $scope.lang = langService.lang;
+    }
+]);
